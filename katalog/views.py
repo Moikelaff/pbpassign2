4c254bc1b05ad4b7c4b05c9031ae40a204746859
Dylan Adiprawira <Dylan.adiprawira@gmail.com>
2022-09-15 11:37:37 +0700

    tugas 2 katalog done

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
+def show_katalog(request):
+    data_katalog = CatalogItem.objects.all()
+    context = {
+        'list_katalog' : data_katalog,
+        'nama' : 'dylan adiprawira',
+        'student_id' : '2106750446'
+    }
+
+    return render(request, 'katalog.html', context)
 # TODO: Create your views here.
