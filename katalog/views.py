0c479696b8e4c180e364e18492ebaccce9ff352c
Dylan Adiprawira <Dylan.adiprawira@gmail.com>
2022-09-19 11:40:35 +0700

    deploy

--- katalog/views.py
+++ katalog/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from katalog.models import CatalogItem
 
+
+# ini katalog
 def show_katalog(request):
     data_katalog = CatalogItem.objects.all()
     context = {
